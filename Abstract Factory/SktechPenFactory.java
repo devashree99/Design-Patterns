df2e211ff74d4d1d6ee9ad0b91bc18d1383b7181
Devashree Ambegaokar <98074112+devashree99@users.noreply.github.com>
2023-09-06 23:40:53 +0530

    Update SktechPenFactory.java

--- Abstract Factory/SktechPenFactory.java
+++ Abstract Factory/SktechPenFactory.java
@@ -1,16 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package abstractfactory;
 
-/**
- *
- * @author HP
- */
-public class SktechPenFactory implements PenFactory {
-
-    public SktechPenFactory() {
-    }
-    
-}
